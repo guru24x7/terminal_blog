fbddea0c004ff59e59c1742b61962f06fa284f08
gaming guru <gaming.guru24x7@gmail.com>
2016-09-02 16:31:36 +0500

    Initial Commit

---
+++ database.py
@@ -0,0 +1,18 @@
+import pymongo
+
+
+class Database(object):
+    URI = "mongodb://192.168.126.128:27017"
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['fullstack']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+
+

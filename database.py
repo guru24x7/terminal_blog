7f6ab685dc9ca2a31d9fa8d878618c2c99097e4f
gaming guru <gaming.guru24x7@gmail.com>
2016-09-05 21:20:23 +0500

    database py

--- database.py
+++ database.py
@@ -14,5 +14,10 @@ class Database(object):
     def insert(collection, data):
         Database.DATABASE[collection].insert(data)
 
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
 
-
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
